32a984f05bdd8e2e4236a2def9892e9b761d2994
rpshbhandari <rpshbhandari@gmail.com>
2021-04-16 10:25:32 +0545

    update

--- Scripts/dd.py
+++ Scripts/dd.py
@@ -20,7 +20,7 @@ deaths = []
 try:
     for date in dates:
         for country in countries:
-            querystring = {"country":f"{country}","day":"2020-06-15"}
+            querystring = {"country":f"{country}","day":f"{date}"}
             response = requests.request("GET", url, headers=headers, params=querystring)
             d = response.json()
             for i in d['response']:
@@ -30,10 +30,10 @@ try:
                 df_deaths = pd.DataFrame(deaths)
                 with open(f"{country}/cases/{querystring['day']}.md", 'w') as f:
                     f.write(df_cases.to_markdown())
-                    print(f"{country} 's cases for 2020-06-02 is done!")
+                    print(f"{country} 's cases for {date} is done!")
                 with open(f"{country}/deaths/{querystring['day']}.md", 'w') as f:
                     f.write(df_deaths.to_markdown())
-                    print(f"{country} 's deaths for 2020-06-02 is done!")
+                    print(f"{country} 's deaths for {date} is done!")
             time.sleep(10)
 finally:
     os.system('git add .')
